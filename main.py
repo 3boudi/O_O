b9b987bacf7637d40c2b1717f064a8e2105b154c
3boudi <vinnyhalitimb47@gmail.com>
2025-12-05 11:43:14 +0100

    bismilah

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
 from models import Major, Speciality, User , Role ,Gender
-from uuid import uuid4
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
@@ -18,6 +18,19 @@ db: List[User] = [
         middle_name=None, 
         gender=Gender.MALE,
         roles=[Role.ADMIN]
+    ),
+    User(
+        first_name="Sara",
+        last_name="Ali",
+        Bac_matrucule=2345678901,
+        major=Major.IT,
+        speciality=Speciality.DS,
+        year_of_study=2,
+        section=1,
+        group_number=3,
+        middle_name="M.",
+        gender=Gender.FEMALE,
+        roles=[Role.USER]
     )
 ]
 
@@ -33,3 +46,41 @@ async def get_users():
 async def create_user(user: User):
     db.append(user)
     return user
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_id: UUID, updated_user: User):
+    for user in db :
+        if user.id == user_id:
+            if updated_user.first_name is not None:
+                user.first_name = updated_user.first_name
+            if updated_user.last_name is not None:
+                user.last_name = updated_user.last_name
+            if updated_user.Bac_matrucule is not None:
+                user.Bac_matrucule = updated_user.Bac_matrucule
+            if updated_user.major is not None:
+                user.major = updated_user.major
+            if updated_user.speciality is not None:
+                user.speciality = updated_user.speciality
+            if updated_user.year_of_study is not None:
+                user.year_of_study = updated_user.year_of_study
+            if updated_user.section is not None:
+                user.section = updated_user.section
+            if updated_user.group_number is not None:
+                user.group_number = updated_user.group_number
+            if updated_user.middle_name is not None:
+                user.middle_name = updated_user.middle_name
+            if updated_user.gender is not None:
+                user.gender = updated_user.gender
+            if updated_user.roles is not None:  
+                user.roles = updated_user.roles
+            return user , "updated successfully"
+    raise HTTPException(status_code=404, detail="User not found")
+            
